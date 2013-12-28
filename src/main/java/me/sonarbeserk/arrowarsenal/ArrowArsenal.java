3141d54fa501b9b6e09964f7d6dd5d92d137a597
SonarBeserk <sonarbeserk@gmail.com>
2013-12-28 13:45:26 -0500

    add empty JavaPlugin

---
+++ src/main/java/me/sonarbeserk/arrowarsenal/ArrowArsenal.java
@@ -0,0 +1,16 @@
+package me.sonarbeserk.arrowarsenal;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ArrowArsenal extends JavaPlugin {
+
+    public void onEnable() {
+
+
+    }
+
+    public void onDisable() {
+
+
+    }
+}
