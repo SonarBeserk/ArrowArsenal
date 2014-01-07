e79c21c23679b4182b3df568d750c200c2119963
SonarBeserk <sonarbeserk@gmail.com>
2014-01-07 18:25:24 -0500

    fixed extra persistance

--- src/main/java/me/sonarbeserk/arrowarsenal/ArrowArsenal.java
+++ src/main/java/me/sonarbeserk/arrowarsenal/ArrowArsenal.java
@@ -67,7 +67,6 @@ public class ArrowArsenal extends JavaPlugin {
     public void onDisable() {
 
         data.set("disabled-arrows", ArrowRegistry.getInstance().getDisabledArrowNames());
-        data.set("arrow-list", ArrowRegistry.getInstance().getArrows());
         data.save();
         data = null;
 
