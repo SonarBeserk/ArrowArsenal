f5d3c862a75a6faf29127f1d169d12fbf4de43b7
SonarBeserk <sonarbeserk@gmail.com>
2014-02-26 23:20:53 -0500

    hook vault

--- src/main/java/me/sonarbeserk/arrowarsenal/ArrowArsenal.java
+++ src/main/java/me/sonarbeserk/arrowarsenal/ArrowArsenal.java
@@ -7,9 +7,14 @@ import me.sonarbeserk.arrowarsenal.tracking.PlayerTracker;
 import me.sonarbeserk.arrowarsenal.utils.Messaging;
 import me.sonarbeserk.arrowarsenal.utils.Data;
 import me.sonarbeserk.arrowarsenal.utils.Locale;
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ArrowArsenal extends JavaPlugin {
+
+    public static Economy economy = null;
 
     private Locale locale = null;
 
@@ -42,8 +47,34 @@ public class ArrowArsenal extends JavaPlugin {
         tracker = new PlayerTracker(this);
 
         getServer().getPluginManager().registerEvents(new ArrowListener(this), this);
+
+        if(getServer().getPluginManager().getPlugin("Vault") != null && getServer().getPluginManager().getPlugin("Vault").isEnabled()) {
+
+            setupEconomy();
+
+            if(economy == null) {
+
+                getLogger().warning(ChatColor.stripColor(ChatColor.translateAlternateColorCodes('&', getLocale().getMessage("warning-no-economy-found"))));
+            } else {
+
+                getLogger().info(ChatColor.stripColor(ChatColor.translateAlternateColorCodes('&', getLocale().getMessage("hooked-vault"))));
+            }
+        } else {
+
+            getLogger().warning(ChatColor.stripColor(ChatColor.translateAlternateColorCodes('&', getLocale().getMessage("warning-vault-not-found"))));
+        }
     }
 
+    private boolean setupEconomy() {
+
+        RegisteredServiceProvider<Economy> economyProvider = getServer().getServicesManager().getRegistration(net.milkbowl.vault.economy.Economy.class);
+
+        if(economyProvider != null) {
+            economy = economyProvider.getProvider();
+        }
+
+        return (economy != null);
+    }
 
     /**
      * Returns the locale in use
