c24935b6fabc4ca142a652224d30398c03429057
SonarBeserk <sonarbeserk@gmail.com>
2014-02-26 23:06:41 -0500

    documentation

--- src/main/java/me/sonarbeserk/arrowarsenal/clicklisteners/ClickListener.java
+++ src/main/java/me/sonarbeserk/arrowarsenal/clicklisteners/ClickListener.java
@@ -4,5 +4,9 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 
 public abstract class ClickListener {
 
+    /**
+     * Called when a menu item is clicked
+     * @param e the event the click occured in
+     */
     public abstract void click(InventoryClickEvent e);
 }
