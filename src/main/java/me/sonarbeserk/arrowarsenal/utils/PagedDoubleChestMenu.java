1aff97a2ca0334b680da595bf0cd3e9314afe70c
SonarBeserk <sonarbeserk@gmail.com>
2014-02-27 21:27:32 -0500

    wanted to give thanks

--- src/main/java/me/sonarbeserk/arrowarsenal/utils/PagedDoubleChestMenu.java
+++ src/main/java/me/sonarbeserk/arrowarsenal/utils/PagedDoubleChestMenu.java
@@ -14,6 +14,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Inspired by the menus in GuestCraft http://dev.bukkit.org/bukkit-plugins/guestcraft/
+ */
 public class PagedDoubleChestMenu implements Listener {
 
     private JavaPlugin plugin = null;
