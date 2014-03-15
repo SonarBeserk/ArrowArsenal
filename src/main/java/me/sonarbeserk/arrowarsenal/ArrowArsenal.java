7d5987b484bd615d735d93faf6649f0d345388ad
SonarBeserk <sonarbeserk@gmail.com>
2014-03-14 23:47:06 -0400

    add license to classes themselves

--- src/main/java/me/sonarbeserk/arrowarsenal/ArrowArsenal.java
+++ src/main/java/me/sonarbeserk/arrowarsenal/ArrowArsenal.java
@@ -13,6 +13,30 @@ import org.bukkit.ChatColor;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
+/***********************************************************************************************************************
+ *
+ * ArrowArsenal - Bukkit plugin to provide a system to organize special arrows
+ * ===========================================================================
+ *
+ * Copyright (C) 2013, 2014 by SonarBeserk
+ * http://dev.bukkit.org/bukkit-plugins/arrowarsenal/
+ *
+ ***********************************************************************************************************************
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ***********************************************************************************************************************/
 public class ArrowArsenal extends JavaPlugin {
 
     public static Economy economy = null;
--- src/main/java/me/sonarbeserk/arrowarsenal/arrows/ExampleArrow.java
+++ src/main/java/me/sonarbeserk/arrowarsenal/arrows/ExampleArrow.java
@@ -9,6 +9,30 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/***********************************************************************************************************************
+ *
+ * ArrowArsenal - Bukkit plugin to provide a system to organize special arrows
+ * ===========================================================================
+ *
+ * Copyright (C) 2013, 2014 by SonarBeserk
+ * http://dev.bukkit.org/bukkit-plugins/arrowarsenal/
+ *
+ ***********************************************************************************************************************
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ***********************************************************************************************************************/
 public class ExampleArrow extends SArrow {
 
     ArrowState state = null;
